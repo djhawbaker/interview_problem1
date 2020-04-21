3314c84f2d4eeed03f79bf1a6e8e9a0194527d8f
David Hawbaker <djhawbaker@gmail.com>
2020-04-20 21:21:14 -0700

    Update getValues to get the widget values
    Updated the getValues function to get the user input values for each widget.
    It returns a dictionary containing the widget values with the label as the
    key.

--- basic_dialog.py
+++ basic_dialog.py
@@ -3,41 +3,45 @@
 from PyQt4 import QtGui, QtCore
 
 class BasicDialog(QtGui.QDialog):
+
     def __init__(self, parent=None):
         super(BasicDialog, self).__init__(parent=parent)
         #Instantiate a layout and set it as the dialogs main layout
         main_layout = QtGui.QVBoxLayout()
         self.setLayout(main_layout)
 
-        # Add line edit labelled Name
-        line_label = QtGui.QLabel("Name")
-        main_layout.addWidget(line_label)
+        # Add line edit labelled "Name"
+        name_label = QtGui.QLabel("Name")
+        main_layout.addWidget(name_label)
 
-        line_edit = QtGui.QLineEdit()
-        main_layout.addWidget(line_edit)
+        self.name_edit = QtGui.QLineEdit()
+        main_layout.addWidget(self.name_edit)
 
-        # Add combo box labelled Type with options A, B, C
+        # Add combo box labelled "Type" with options A, B, C
         combo_label = QtGui.QLabel("Type")
         main_layout.addWidget(combo_label)
-        combo_box = QtGui.QComboBox()
-        combo_box.addItem("A")
-        combo_box.addItem("B")
-        combo_box.addItem("C")
-        main_layout.addWidget(combo_box)
+
+        self.combo_box = QtGui.QComboBox()
+        self.combo_box.addItem("A")
+        self.combo_box.addItem("B")
+        self.combo_box.addItem("C")
+        main_layout.addWidget(self.combo_box)
 
         # Add Date widget labelled "Need By" - Defaults to one week in the future.
         # Can't modify to a date in the past
         date_label = QtGui.QLabel("Need By")
         main_layout.addWidget(date_label)
-        dateWidget = QtGui.QDateEdit(QtCore.QDate.currentDate().addDays(7))
-        dateWidget.setMinimumDate(QtCore.QDate.currentDate())
-        main_layout.addWidget(dateWidget) 
 
-        # Text Edit labelled Notes
+        self.dateWidget = QtGui.QDateEdit(QtCore.QDate.currentDate().addDays(7))
+        self.dateWidget.setMinimumDate(QtCore.QDate.currentDate())
+        main_layout.addWidget(self.dateWidget) 
+
+        # Text Edit labelled "Notes"
         text_label = QtGui.QLabel("Notes")
         main_layout.addWidget(text_label)
-        text_edit = QtGui.QTextEdit()
-        main_layout.addWidget(text_edit) 
+
+        self.note_edit = QtGui.QTextEdit()
+        main_layout.addWidget(self.note_edit) 
 
         #Add ok and cancel buttons
         #And hook them up to dialogs accept/reject methods
@@ -48,7 +52,17 @@ class BasicDialog(QtGui.QDialog):
         main_layout.addWidget(button_box)
 
     def getValues(self):
-        return None
+        """Gets the current values of each widget
+        
+        Returns:
+            Dictionary of all widget values with the label as the key
+        """
+        d = {}
+        d['Name'] = str(self.name_edit.text())
+        d['Type'] = str(self.combo_box.currentText())
+        d['Need By'] = str(self.dateWidget.date().toString('yyyy-MM-dd'))
+        d['Notes'] = str(self.note_edit.toPlainText())
+        return d
 
 if __name__ == '__main__':
     #Instantiate a QApplication - requirement for Qt
