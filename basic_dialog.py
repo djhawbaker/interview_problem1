cc6f0e9f47cba90cf5570fe1787926aaffd84561
David Hawbaker <djhawbaker@gmail.com>
2020-04-20 20:47:09 -0700

    Add widget structure to basic_dialog
    Added a line edit labelled "Name"
    Added a combo box labelled "Type" with options A, B, C
    Added a date widget labelled "Need By" that defaults to a week in the future
    It also doesn't let the user modify to a date in the past
    Added a text edit labelled "Notes"

--- basic_dialog.py
+++ basic_dialog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 class BasicDialog(QtGui.QDialog):
     def __init__(self, parent=None):
@@ -8,6 +8,37 @@ class BasicDialog(QtGui.QDialog):
         #Instantiate a layout and set it as the dialogs main layout
         main_layout = QtGui.QVBoxLayout()
         self.setLayout(main_layout)
+
+        # Add line edit labelled Name
+        line_label = QtGui.QLabel("Name")
+        main_layout.addWidget(line_label)
+
+        line_edit = QtGui.QLineEdit()
+        main_layout.addWidget(line_edit)
+
+        # Add combo box labelled Type with options A, B, C
+        combo_label = QtGui.QLabel("Type")
+        main_layout.addWidget(combo_label)
+        combo_box = QtGui.QComboBox()
+        combo_box.addItem("A")
+        combo_box.addItem("B")
+        combo_box.addItem("C")
+        main_layout.addWidget(combo_box)
+
+        # Add Date widget labelled "Need By" - Defaults to one week in the future.
+        # Can't modify to a date in the past
+        date_label = QtGui.QLabel("Need By")
+        main_layout.addWidget(date_label)
+        dateWidget = QtGui.QDateEdit(QtCore.QDate.currentDate().addDays(7))
+        dateWidget.setMinimumDate(QtCore.QDate.currentDate())
+        main_layout.addWidget(dateWidget) 
+
+        # Text Edit labelled Notes
+        text_label = QtGui.QLabel("Notes")
+        main_layout.addWidget(text_label)
+        text_edit = QtGui.QTextEdit()
+        main_layout.addWidget(text_edit) 
+
         #Add ok and cancel buttons
         #And hook them up to dialogs accept/reject methods
         button_box = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok|\
